83027d8123b01988ab9401fe17ab3d523e7cccd0
Marc Gravell <marc.gravell@gmail.com>
2025-03-21 09:08:23 +0000

    add benchmark before changes

---
+++ test/Npgsql.Benchmarks/CommandCreationBenchmarks.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Npgsql.Benchmarks;
+
+[Config(typeof(Config))]
+public class CommandCreationBenchmarks
+{
+    [Params(0,1,5,10,20)]
+    public int ParameterCount { get; set; }
+
+    [Benchmark]
+    public void CreateCommand()
+    {
+        using var cmd = new NpgsqlCommand("""select * from orders""");
+        var paramCount = ParameterCount;
+        for (var i = 0; i < paramCount; i++)
+        {
+            cmd.Parameters.Add(new NpgsqlParameter<int> { TypedValue = i });
+        }
+        if (cmd.Parameters.Count != paramCount)
+        {
+            Throw();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining), DoesNotReturn]
+        static void Throw() => throw new InvalidOperationException("Incorrect parameter count detected");
+    }
+
+    class Config : ManualConfig
+    {
+        public Config()
+        {
+            AddColumn(StatisticColumn.OperationsPerSecond);
+            AddDiagnoser(MemoryDiagnoser.Default);
+        }
+    }
+}
